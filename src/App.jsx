97756e79421d44693eb66522188749dec5824c2c
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-16 20:13:02 +0600

    button click

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ function handleClick (){
   alert('button click')
 }
 
+const handleClick2=() =>{
+  alert('button click 2')
+}
+
 function App() {
 
   return (
     <>
     <h3>React core concepts </h3>
     <button onClick={handleClick}>click Me</button>
+    <button onClick={handleClick2}>Click 2</button>
+    <button onClick={() =>{alert('button click3')}}>third</button>
     </>
   )
 }
