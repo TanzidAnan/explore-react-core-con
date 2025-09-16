8b9088d949e47e4ed043c713d4e757e8a01f8c6e
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-16 21:47:47 +0600

    handle add

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,13 +2,41 @@ import React, { useState } from 'react';
 
 
 
+
 const Counter = () => {
-    const [count,setCount] =useState(0)
+    const [count, setCount] = useState(0)
+
+    const handleAdd = () => {
+        const newCount = count + 1;
+        setCount(newCount)
+    }
+    const handleReduce=() =>{
+        const newCount =count -1;
+        setCount(newCount)
+    }
     return (
-        <div>
-            <h3>Counter: </h3>
+        <div style={{ border: '2px solid red', borderRadius: '5px' }}>
+            <h3>Counter: {count}</h3>
+            <button onClick={handleAdd}>Add button</button>
+            <button onClick={handleReduce}>reduch</button>
         </div>
     );
 };
 
 export default Counter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
