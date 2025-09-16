c23f1871edd89ebf0a538bc9a62931177a90573d
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-16 20:17:03 +0600

    click me function

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 
 import './App.css'
 
-function handleClick (){
+function handleClick() {
   alert('button click')
 }
 
-const handleClick2=() =>{
+const handleClick2 = () => {
   alert('button click 2')
+}
+
+const addToFive = (num) => {
+  alert(num + 5)
+
 }
 
 function App() {
 
   return (
     <>
-    <h3>React core concepts </h3>
-    <button onClick={handleClick}>click Me</button>
-    <button onClick={handleClick2}>Click 2</button>
-    <button onClick={() =>{alert('button click3')}}>third</button>
+      <h3>React core concepts </h3>
+      <button onClick={handleClick}>click Me</button>
+      <button onClick={handleClick2}>Click 2</button>
+      <button onClick={() => { alert('button click3') }}>third</button>
+      <button onClick={()=>addToFive(5)}>four</button>
     </>
   )
 }
